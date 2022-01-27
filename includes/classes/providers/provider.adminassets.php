818d45c96f802aa9df392546348edbbf92eceb0a
Raphael Batista Fontão <rbfraphael@gmail.com>
2022-01-27 19:45:00 -0300

    Referencing minified CSS files

--- includes/classes/providers/provider.adminassets.php
+++ includes/classes/providers/provider.adminassets.php
@@ -10,7 +10,7 @@ class Provider_AdminAssets
 
     public function register_assets()
     {
-        wp_register_style("starter-theme-admin", STARTERTHEME_URL."/assets/dist/css/admin.css", [], md5(filemtime((STARTERTHEME_PATH."/assets/dist/css/admin.css"))));
+        wp_register_style("starter-theme-admin", STARTERTHEME_URL."/assets/dist/css/admin.min.css", [], md5(filemtime((STARTERTHEME_PATH."/assets/dist/css/admin.min.css"))));
         wp_register_script("starter-theme-admin", STARTERTHEME_URL."/assets/dist/js/admin.js", [], md5(filemtime((STARTERTHEME_PATH."/assets/dist/js/admin.js"))), true);
     }
     
--- includes/classes/providers/provider.assets.php
+++ includes/classes/providers/provider.assets.php
@@ -10,7 +10,7 @@ class Provider_Assets
 
     public function register_assets()
     {
-        wp_register_style("starter-theme", STARTERTHEME_URL."/assets/dist/css/theme.css", [], md5(filemtime((STARTERTHEME_PATH."/assets/dist/css/theme.css"))));
+        wp_register_style("starter-theme", STARTERTHEME_URL."/assets/dist/css/theme.min.css", [], md5(filemtime((STARTERTHEME_PATH."/assets/dist/css/theme.min.css"))));
         wp_register_script("starter-theme", STARTERTHEME_URL."/assets/dist/js/theme.js", [], md5(filemtime((STARTERTHEME_PATH."/assets/dist/js/theme.js"))), true);
     }
     
