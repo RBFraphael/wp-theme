3a236d42ab3bf693cef3614ae027594428c5cbad
Raphael Batista Fontão <rbfraphael@gmail.com>
2022-01-27 11:05:43 -0300

    Updated gulp tasks, ignored files and node dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const imagemin = require('gulp-imagemin');
 const browserify = require('browserify');
 const babelify = require('babelify');
 const source = require('vinyl-source-stream');
+const purgecss = require('gulp-purgecss');
+const rename = require('gulp-rename');
 
 /**
  * 
@@ -12,29 +14,33 @@ const source = require('vinyl-source-stream');
  * 
  */
 
-gulp.task("css:build", (done) => {
-    gulp.src("./assets/src/scss/admin/admin.scss")
+gulp.task("css:build", () => {
+    return gulp.src(["./assets/src/scss/admin/admin.scss", "./assets/src/scss/theme/theme.scss"])
         .pipe(sass.sync({
             outputStyle: "compressed"
         }).on("error", sass.logError))
         .pipe(gulp.dest("./assets/dist/css"));
-    
-    gulp.src("./assets/src/scss/theme/theme.scss")
-        .pipe(sass.sync({
-            outputStyle: "compressed"
-        }).on("error", sass.logError))
+});
+
+gulp.task("css:purge", () => {
+    return gulp.src("./assets/dist/css/*.css")
+        .pipe(purgecss({
+            content: [
+                "./*.php",
+                "./templates/**/*.php"
+            ]
+        }))
+        .pipe(rename({
+            suffix: ".min"
+        }))
         .pipe(gulp.dest("./assets/dist/css"));
-
-    done();
 });
 
-gulp.task("css:watch", (done) => {
-    gulp.watch("./assets/src/scss/**/*.scss", gulp.series("css:build"));
-    
-    done();
+gulp.task("css:watch", () => {
+    return gulp.watch("./assets/src/scss/**/*.scss", gulp.series("css:build", "css:purge"));
 });
 
-gulp.task("css", gulp.series("css:build"));
+gulp.task("css", gulp.series("css:build", "css:purge"));
 
 /**
  * 
@@ -43,7 +49,7 @@ gulp.task("css", gulp.series("css:build"));
  */
 
 gulp.task("js:build", (done) => {
-    browserify({entries: ['./assets/src/js/theme/theme.js']})
+    browserify({entries: './assets/src/js/theme/theme.js'})
         .transform(babelify.configure({
             presets: ['@babel/preset-env']
         }))
@@ -62,10 +68,8 @@ gulp.task("js:build", (done) => {
     done();
 });
 
-gulp.task("js:watch", (done) => {
-    gulp.watch("./assets/src/js/**/*.js", gulp.series("js:build"));
-    
-    done();
+gulp.task("js:watch", () => {
+    return gulp.watch("./assets/src/js/**/*.js", gulp.series("js:build"));
 });
 
 gulp.task("js", gulp.series("js:build"));
@@ -76,18 +80,14 @@ gulp.task("js", gulp.series("js:build"));
  * 
  */
 
-gulp.task("img:build", (done) => {
-    gulp.src("./assets/src/img/*")
+gulp.task("img:build", () => {
+    return gulp.src("./assets/src/img/*")
         .pipe(imagemin())
         .pipe(gulp.dest("./assets/dist/img"));
-    
-    done();
 });
 
 gulp.task("img:watch", (done) => {
-    gulp.watch("./assets/src/img/*", gulp.series("img:build"));
-
-    done();
+    return gulp.watch("./assets/src/img/*", gulp.series("img:build"));
 });
 
 gulp.task("img", gulp.series("img:build"));
@@ -101,20 +101,18 @@ gulp.task("img", gulp.series("img:build"));
 gulp.task("fonts:copy", (done) => {
     gulp.src("./assets/src/fonts/*")
         .pipe(gulp.dest("./assets/dist/fonts"));
-    
+
     gulp.src("./assets/src/webfonts/*")
         .pipe(gulp.dest("./assets/dist/webfonts"));
     
     done();
 });
 
-gulp.task("fonts:watch", (done) => {
-    gulp.watch([
+gulp.task("fonts:watch", () => {
+    return gulp.watch([
         "./assets/src/fonts/*",
         "./assets/src/webfonts/*",
     ], gulp.series("fonts:copy"));
-
-    done();
 });
 
 gulp.task("fonts", gulp.series("fonts:copy"));
