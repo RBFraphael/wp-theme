1180e75f5a433bf76a7d41a742c4974edd8d61df
RBFraphael <rbfraphael@gmail.com>
2024-05-23 22:57:24 -0300

    Added Lenis Scroll and fixed AOS SASS

--- src/assets/scripts/Init.js
+++ src/assets/scripts/Init.js
@@ -4,9 +4,40 @@ export class Init
 {
     constructor ()
     {
-        Aos.init({
-            duration: 1000,
-            easing: "ease-in-out",
-        });
+        window.onload = () => {
+            setTimeout(() => {
+                this.init();
+            }, 1000);
+        };
+
+        this.smoothScroll();
+    }
+
+    init()
+    {
+        setTimeout(() => {
+            Aos.init({
+                duration: 1000,
+                easing: "ease-in-out",
+                once: true,
+                offset: 200,
+            });
+            
+            window.aosRefresh = setInterval(() => {
+                Aos.refresh();
+            }, 500);
+        }, 500);
+    }
+
+    smoothScroll()
+    {
+        window.lenis = new Lenis();
+
+        window.lenisRaf = (time) => {
+            window.lenis.raf(time);
+            requestAnimationFrame(window.lenisRaf);
+        };
+
+        requestAnimationFrame(window.lenisRaf);
     }
 }
